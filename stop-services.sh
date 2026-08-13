3c5ef3bd924143efee271d9f87536f76c3375401
Zubonex <aritrabanerjee1911@gmail.com>
2026-08-13 13:18:26 +0000

    Updated start and stop services

--- stop-services.sh
+++ stop-services.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-docker stop jaeger prometheus alertmanager grafana redis redis-exporter
-echo "All services stopped"
+docker stop jaeger prometheus alertmanager grafana \
+  redis redis-exporter \
+  postgres postgres-exporter \
+  loki promtail
+echo "All services stopped."
+echo "Reminder: also Ctrl+C your ./run.sh terminal and any leak scripts still running."
