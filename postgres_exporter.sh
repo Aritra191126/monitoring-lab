b7cd468b912015d95fc939cfce4923ae34805ac9
Zubonex <aritrabanerjee1911@gmail.com>
2026-08-14 15:50:50 +0000

    Cleaning up

--- postgres_exporter.sh
+++
@@ -1,4 +0,0 @@
-docker run -d --name postgres-exporter \
-  -p 9187:9187 \
-  -e DATA_SOURCE_NAME="postgresql://postgres:labpassword@172.17.0.1:5432/labdb?sslmode=disable" \
-  prometheuscommunity/postgres-exporter:latest
